ff0685a466870d64da76935539fd513859f3dcc5
Paulo Bahia <pauloipatinga22@gmail.com>
2024-06-30 11:09:29 -0300

    Feat: Shadcn e Login Form

---
+++ src/pages/SignIn/components/LoginForm/index.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Separator } from "@/components/ui/separator"
+
+export const LoginForm = () => {
+  return (
+    <div className="sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="px-4 py-10 mx-2 sm:px-10">
+        <div className="space-y-5">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" id="email" placeholder="john.doe@example.com" className="text-xs border" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="password">Senha</Label>
+            <Input type="password" id="password" placeholder="•••••••••••••" className="text-xs" />
+            <div className='flex justify-start w-full'>
+              <p className='text-xs font-semibold text-white cursor-pointer text-end hover:underline-offset-1'>
+                <div className="cursor-pointer">
+                  Esqueceu a senha?
+                </div>
+              </p>
+            </div>
+          </div>
+          <div>
+            <Button size={'sm'} className="bg-[#6D28D9] hover:bg-[#6D28D9]/50 text-white w-full">
+              Iniciar sessão
+            </Button>
+          </div>
+        </div>
+        <Separator className="my-4" />
+        <div className="flex flex-col space-y-3">
+          <Button size={'sm'} variant={'outline'}>
+            Entrar com Google
+          </Button>
+          <Button size={'sm'} variant={'outline'}>
+            Entrar com SAML/OIDC
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,14 @@
+import { LoginForm } from "./components"
+
 export const SignIn = () => {
   return (
-    <main className="flex justify-center min-h-screen antialiased bg-background">
-
-    </main>
+    <main className="flex flex-col items-center justify-center min-h-screen p-10 antialiased bg-background">
+      <div className="text-center sm:mx-auto sm:w-full sm:max-w-md">
+        <span className="text-3xl font-medium text-center">
+          Bem-vindo(a) novamente
+        </span>
+      </div>
+      <LoginForm />
+    </main >
   )
 }
