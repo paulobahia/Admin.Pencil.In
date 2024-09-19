b1a5cde9668e90f91324d8ea4471f45c14c90faa
Paulo Bahia <pauloipatinga22@gmail.com>
2024-09-19 14:25:10 -0300

    Feat: Roteamento e criado array moreItens

--- src/pages/Dashboard/More/index.tsx
+++ src/pages/Dashboard/More/index.tsx
@@ -1,28 +1,39 @@
 import { Card } from "@/components/ui/card"
 import { ChevronRight, LucideLineChart, Zap } from "lucide-react"
+import { NavDasboardItem } from "../types"
+import { Link } from "react-router-dom"
+
+const moreItens: NavDasboardItem[] = [
+  {
+    label: 'Automações',
+    path: '/automations',
+    icon: <Zap size={16} />
+  },
+  {
+    label: 'Análise',
+    path: '/insights',
+    icon: <LucideLineChart size={16} />
+  }
+]
 
 export const More = () => {
   return (
-    <main className="flex flex-col flex-1 antialiased bg-background">
-      <Card className="flex flex-row items-center justify-between p-5 border-b-0 rounded-md rounded-t-none rounded-b-none cursor-pointer text-muted-foreground hover:bg-foreground/5 first:rounded-t-md last:rounded-b-md last:border-b">
-        <div className="flex flex-row items-center gap-x-2">
-          <Zap size={16} />
-          <span className="text-sm font-semibold">
-            Automação
-          </span>
-        </div>
-        <ChevronRight size={16} />
-      </Card>
-      <Card className="flex flex-row items-center justify-between p-5 border-b-0 rounded-md rounded-t-none rounded-b-none cursor-pointer text-muted-foreground hover:bg-foreground/5 first:rounded-t-md last:rounded-b-md last:border-b">
-        <div className="flex flex-row items-center gap-x-2">
-          <LucideLineChart size={16} />
-          <span className="text-sm font-semibold">
-            Análise
-          </span>
-        </div>
-        <ChevronRight size={16} />
-      </Card>
-      <span className="mt-6 text-xs leading-tight text-muted-foreground">
+    <main className="flex flex-col flex-1 antialiased bg-background group">
+      {moreItens.map(({ path, label, icon }) => (
+        <Link key={path} to={path}>
+          <Card className="flex flex-row items-center justify-between p-5 border-b rounded-none cursor-pointer text-muted-foreground hover:bg-foreground/5">
+            <div className="flex flex-row items-center gap-x-2">
+              {icon}
+              <span className="text-sm font-semibold">
+                {label}
+              </span>
+            </div>
+            <ChevronRight size={16} />
+          </Card>
+        </Link>
+      )
+      )}
+      <span className="mt-3 text-xs leading-tight text-muted-foreground">
         Pense na nossa aplicação móvel como uma pequena extensão do nosso aplicativo web. Para realizar ações mais complexas, utilize a versão web.
       </span>
     </main>
