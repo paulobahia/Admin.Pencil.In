c331645bc888b385798dc459e52218dac04a8449
Paulo Bahia <pauloipatinga22@gmail.com>
2024-09-19 14:36:05 -0300

    Enable Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import './globals.css'
 import { ThemeProvider } from './components/theme-provider'
 import { RouterProvider } from 'react-router-dom'
 import { Routers } from './routes'
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <RouterProvider router={Routers} />
+      <SpeedInsights />
     </ThemeProvider>
   </React.StrictMode>,
 )
