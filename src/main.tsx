c97a641f4d00aeb41ec45a6004b9e2810c708104
Paulo Bahia <pauloipatinga22@gmail.com>
2024-09-19 14:38:04 -0300

    Enable Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import { ThemeProvider } from './components/theme-provider'
 import { RouterProvider } from 'react-router-dom'
 import { Routers } from './routes'
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <RouterProvider router={Routers} />
+      <Analytics />
       <SpeedInsights />
     </ThemeProvider>
   </React.StrictMode>,
